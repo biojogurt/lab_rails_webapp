25ed2f3dfae97821c564c40758c9798c5b45f04d
Polina Amelina <anathebeequeen@gmail.com>
2023-06-02 14:36:26 +0300

    add db to volume and fix sql script

--- init.sql
+++ init.sql
@@ -1,2 +1,9 @@
-DROP ROLE IF EXISTS blog_development;
-CREATE USER blog_development SUPERUSER PASSWORD 'blog_development';
+DO
+$$
+BEGIN
+    IF NOT EXISTS (SELECT * FROM pg_user WHERE usename = 'blog_development') THEN
+        CREATE ROLE blog_development SUPERUSER PASSWORD 'blog_development';
+    END IF;
+END
+$$
+;
